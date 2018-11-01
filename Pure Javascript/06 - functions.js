f85ab9f0b27f7421b0c2243d771986345f69ed9e
David van Driessche <david.van.driessche@fourpees.com>
2018-11-01 15:06:09 +0100

    Add functions snippet

---
+++ Pure Javascript/06 - functions.js
@@ -0,0 +1,14 @@
+// A function in Javascript is a way to encapsulate common code
+function addValues( value1, value2, value3 ) {
+
+        // In the function body, you can do anything you want
+
+        // If you want to pass back a result, use "return"
+        return value1 + value2 + value3;
+}
+
+// Using a function simply uses the name
+var theResult = addValues( 1, 2, 3 );
+
+// And as the "+" operator in JavaScript also works for strings...
+var theResult2 = addValues( "Penguins ", "where ", "here!" );
