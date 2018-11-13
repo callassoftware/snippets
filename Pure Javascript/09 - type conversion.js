f5001c206f72d4d0089545507c6feb2bb99c52c2
Vincent Lepski <Vinnecent@Karl.local>
2018-11-13 11:19:20 +0100

    type conversion

---
+++ Pure Javascript/09 - type conversion.js
@@ -0,0 +1,21 @@
+/**************************************************************************************************/
+/* Converting numbers to strings */
+/**************************************************************************************************/
+
+var num = 56;
+String(num);		// returns a string from a number variable num
+String(56);			// returns a string from a number literal 123
+
+// The number Method toString() does the same.
+
+number.toString();
+(56).toString();
+
+
+/**************************************************************************************************/
+/* Converting strings to numbers */
+/**************************************************************************************************/
+
+var str = "5.6";	
+Number(str);		// returns a number from a string variable str
+Number('56')		// returns a number from a string literal '56'
